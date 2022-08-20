d33549064a114cb313ce79c7ca28d31f116d2c72
MM <wambua@medicmobile.org>
2022-08-20 10:56:28 +0300

    analyze

--- models/brac/indexes.sql
+++ models/brac/indexes.sql
@@ -1,6 +1,6 @@
 {{ config(schema='v1', materialized = 'raw_sql'), 
-"post-hook" : [
-      after_commit("vacuum {{this.schema}}.couchdb")
+"post_hook"=[
+      "ANALYZE {{this.schema}}.couchdb"
  }} 
 
 CREATE INDEX IF NOT EXISTS couchdb_doc_id ON {{ env_var('ROOT_POSTGRES_SCHEMA') }}.{{ env_var('POSTGRES_TABLE') }} ((doc ->> '_id'::text) text_ops);
